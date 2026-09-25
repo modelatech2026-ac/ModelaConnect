b3a5a548848be9910e305a9d2587b87091f2fa1f
modelatech2026-ac <modelatech2026@gmail.com>
2026-09-25 17:08:03 +0530

    Update ToastContext.tsx

--- context/ToastContext.tsx
+++ context/ToastContext.tsx
@@ -1,1 +1,15 @@
+import React, { createContext, useContext } from 'react';
 
+const ToastContext = createContext({ showToast: (msg: string) => {} });
+
+export const useToast = () => useContext(ToastContext);
+
+export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ToastContext.Provider value={{ showToast: (msg) => console.log(msg) }}>
+      {children}
+    </ToastContext.Provider>
+  );
+};
+
+export default ToastContext;
